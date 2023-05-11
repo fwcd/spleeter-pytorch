d67efdf2323f0444a8e8f5b010e1549c9ec77dd0
fwcd <fwcdmail@gmail.com>
2023-05-11 16:44:16 +0200

    Use complex STFT as recommended

--- spleeter_pytorch/estimator.py
+++ spleeter_pytorch/estimator.py
@@ -31,22 +31,20 @@ class Estimator(nn.Module):
         """
 
         stft = torch.stft(wav, n_fft=self.win_length, hop_length=self.hop_length, window=self.win,
-                          center=True, return_complex=False, pad_mode='constant')
+                          center=True, return_complex=True, pad_mode='constant')
 
         # only keep freqs smaller than self.F
-        stft = stft[:, :self.F, :, :]
-        real = stft[:, :, :, 0]
-        im = stft[:, :, :, 1]
-        mag = torch.sqrt(real ** 2 + im ** 2)
+        stft = stft[:, :self.F, :]
+        mag = stft.abs()
 
-        return stft, mag
+        return torch.view_as_real(stft), mag
 
     def inverse_stft(self, stft):
         """Inverses stft to wave form"""
 
         pad = self.win_length // 2 + 1 - stft.size(1)
         stft = F.pad(stft, (0, 0, 0, 0, 0, pad))
-        stft = torch.complex(stft[:, :, :, 0], stft[:, :, :, 1])
+        stft = torch.view_as_complex(stft)
         wav = torch.istft(stft, self.win_length, hop_length=self.hop_length, center=True,
                     window=self.win)
         return wav.detach()
@@ -62,7 +60,7 @@ class Estimator(nn.Module):
             wav (tensor): 2 x L
         """
 
-        # stft - 2 X F x L x 2
+        # stft (complex tensor) - 2 X F x L
         # stft_mag - 2 X F x L
         # Compute the STFT from the mixed wav
         stft, stft_mag = self.compute_stft(wav)
