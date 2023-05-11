3d5d531167d07e1050c33dd13621ed3afd7652e3
fwcd <fwcdmail@gmail.com>
2023-05-11 14:55:13 +0200

    Convert only separator to Core ML

--- to_mlmodel.py
+++ to_mlmodel.py
@@ -14,36 +14,29 @@ import soundfile
 import torch
 
 from pathlib import Path
-from spleeter.estimator import Estimator
+from spleeter_pytorch.estimator import Estimator
 
 ROOT = Path(__file__).resolve().parent
 
 def main():
-    sr = 44100
-    es = Estimator(2, ROOT / 'checkpoints' / '2stems' / 'model')
-    es.eval()
+    samplerate = 44100
+    estimator = Estimator(2, ROOT / 'checkpoints' / '2stems' / 'model')
+    estimator.eval()
 
-    # load wav audio
-    wav, _ = librosa.load(ROOT / 'audio_example.mp3', mono=False, res_type='kaiser_fast',sr=sr)
+    # Load wav audio
+    wav, _ = librosa.load(ROOT / 'audio_example.mp3', mono=False, res_type='kaiser_fast', sr=samplerate)
     wav = torch.Tensor(wav)
 
-    # normalize audio
-    # wav_torch = wav / (wav.max() + 1e-8)
+    # Reproduce the STFT step (which we cannot convert to Core ML, unfortunately)
+    _, stft_mag = estimator.compute_stft(wav)
 
     print('==> Tracing')
-    traced_model = torch.jit.trace(es, wav)
-    print('==> Inferring')
-    out = traced_model(wav)
-    print('==> Converting')
-    mlmodel = ct.convert(traced_model, convert_to='mlprogram', inputs=[ct.TensorType(shape=wav.shape)])
-    print(mlmodel)
+    traced_model = torch.jit.trace(estimator.separator, stft_mag)
+    out = traced_model(stft_mag)
 
-    wavs = es.separate(wav)
-    for i in range(len(wavs)):
-        fname = 'output/out_{}.wav'.format(i)
-        print('Writing ',fname)
-        soundfile.write(fname, wavs[i].cpu().detach().numpy().T, sr, "PCM_16")
-        # write_wav(fname, np.asfortranarray(wavs[i].squeeze().numpy()), sr)
+    print('==> Converting') # TODO: Dynamic input size?
+    mlmodel = ct.convert(traced_model, convert_to='mlprogram', inputs=[ct.TensorType(shape=stft_mag.shape)])
+    print(mlmodel)
 
 if __name__ == '__main__':
     main()
